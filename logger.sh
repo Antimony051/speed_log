d67d81d44e63f19dcc5c46dc215aacdc3c637d60
Antimony051 <72787497+Antimony051@users.noreply.github.com>
2023-08-28 02:44:53 +0000

    Add files via upload

---
+++ logger.sh
@@ -0,0 +1,3 @@
+date +%w" "%D" "%H:%M >> /home/antimony/Desktop/speed_logs/log 2>> /home/antimony/Desktop/speed_logs/errors
+/home/antimony/.nvm/versions/node/v18.10.0/bin/fast --json >> /home/antimony/Desktop/speed_logs/log 2>> /home/antimony/Desktop/speed_logs/errors
+sleep 10
